2a9ffc904f4c594d188fab78cd954920d7632426
Cyb0nix <cedric.yoganathan@efrei.net>
2024-06-28 15:26:24 +0200

    Init

--- src/shared/api-enums.ts
+++ src/shared/api-enums.ts
@@ -1,28 +1,9 @@
 export enum UserRoles {
   admin = 'admin',
-  member = 'user'
+  member = 'member'
 }
 
 export enum RoleWeight {
   admin,
   member,
-}
-
-export enum Category {
-  SOUND = 'Sound',
-  VIDEO = 'Video',
-  LIGHT = 'Light',
-  ELECTRICITY = 'Electricity',
-}
-
-export enum ItemType {
-  CABLE = 'Cable',
-  CAMERA = 'Camera',
-  MICROPHONE = 'Microphone',
-  ACCESSORY = 'Accessory',
-}
-
-export enum ContainerType {
-  FLIGHT_CASE = 'Flight case',
-  BOX = 'Box',
 }
