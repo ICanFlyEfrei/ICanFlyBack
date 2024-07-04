5f8c9ef2d56110d7d8dddc16212a7db94020ee82
Cyb0nix <cedric.yoganathan@efrei.net>
2024-07-04 14:25:28 +0200

    4-flask-connection

--- src/price-model-api/price-model-api.module.ts
+++ src/price-model-api/price-model-api.module.ts
@@ -2,11 +2,14 @@ import { Module } from '@nestjs/common';
 import { PriceModelApiController } from './controller/price-model-api.controller';
 import { PriceModelApiService } from './service/price-model-api.service';
 import { HttpModule } from '@nestjs/axios';
+import { PriceModelApiRepository } from './repository/price-model-api.repository';
 
 @Module({
-  imports: [HttpModule],
+  imports: [HttpModule.register({
+    timeout: 5000,
+    maxRedirects: 5,
+  }),],
   controllers: [PriceModelApiController],
-  providers: [PriceModelApiService],
-  exports: [PriceModelApiService]
+  providers: [PriceModelApiService,PriceModelApiRepository]
 })
 export class PriceModelApiModule {}
--- src/price-model-api/service/price-model-api.service.ts
+++ src/price-model-api/service/price-model-api.service.ts
@@ -1,13 +1,17 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { PriceModelApiRepository } from '../repository/price-model-api.repository';
 import { PriceModelInputDto } from '../dto/price-model-input.dto';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class PriceModelApiService {
   constructor(
+    @InjectRepository(PriceModelApiRepository)
     private readonly priceModelApiRepository: PriceModelApiRepository,
-    private readonly logger = new Logger(PriceModelApiService.name)
+
   ) {}
+
+  private readonly logger = new Logger(PriceModelApiService.name)
 
   async getPricePrediction(priceModelInput: PriceModelInputDto) {
     try {
