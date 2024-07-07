a56c7e35fa30a3c39bb7805c3d2fe20bc2427eb5
Cyb0nix <cedric.yoganathan@efrei.net>
2024-07-07 13:55:11 +0200

    Update flight creation

--- src/flight/service/flight.service.ts
+++ src/flight/service/flight.service.ts
@@ -6,68 +6,6 @@ import { AircraftTypes, AirlineCodes, Airports, FlightStatus, NumberOfSeats } fr
 import { CreateFlightDto } from '../controller/dto/create-flight.dto';
 import { SearchFlightDTO } from '../controller/dto/search-flight-dto';
 
-class FlightNotFoundException implements Error {
-  constructor(id: string) {
-    this.message = `Flight with id ${id} not found`;
-    this.name = 'FlightNotFoundException';
-  }
-  message: string;
-  name: string;
-}
-class FlightSearchCompanyException implements Error {
-    constructor(company: string) {
-      this.message = `No flights with ${company} found`;
-      this.name = 'FlightSearchCompanyException';
-    }
-    message: string;
-    name: string;
-  }
-
-class FlightSearchStatusException implements Error {
-    constructor(status: string) {
-      this.message = `No flights with ${status} status found`;
-      this.name = 'FlightSearchStatusException';
-    }
-    message: string;
-    name: string;
-  }
-class FlightSearchDepartureException implements Error {
-    constructor(destination: string) {
-      this.message = `No flights departing at ${destination} found`;
-      this.name = 'FlightSearchDepartureException';
-    }
-    message: string;
-    name: string;
-  }
-class FlightSearchDestinationException implements Error {
-    constructor(destination: string) {
-      this.message = `No flights arriving at ${destination} found`;
-      this.name = 'FlightSearchDestinationException';
-    }
-    message: string;
-    name: string;
-  }
-class FlightSearchDepartingDateException implements Error {
-    constructor(date: string) {
-      this.message = `No flight available with this date:${date}`;
-      this.name = 'FlightSearchDepartingDateException';
-    }
-    message: string;
-    name: string;
-  }
-
-class FlightFindAllException implements Error {
-    constructor() {
-      this.message = `Flight search failed`;
-      this.name = 'FlightFindAllException';
-    }
-    message: string;
-    name: string;
-  }
-
-
-
-
 @Injectable()
 export class FlightService{
     constructor(
